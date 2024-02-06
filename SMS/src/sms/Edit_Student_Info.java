512887c62aae9a96b650ec7f0b0724fcbeb88a9d
Keerthika04 <jkeerthi144@gmail.com>
2024-02-06 21:05:36 +0530

    Refactor: Changed the structure of the code by using different classes

---
+++ SMS/src/sms/Edit_Student_Info.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sms;
+
+import java.sql.*;
+import java.util.Scanner;
+import static sms.Dashboard.*;
+import static sms.SMS.*;
+
+public class Edit_Student_Info {
+     private static DB_Connection DB_connection = DB_Connection.get_Instance();
+     
+     public static void editStudent() throws Exception{
+
+        System.out.println("                   ----------------");
+        System.out.println("                   | Edit Student |");  
+        System.out.println("                   ----------------\n");
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("   Enter the Student Id: ");
+        int id = input.nextInt();
+        
+        String query = "Select * from Students where student_id = ?;";
+        try(Connection con = DB_connection.dbConnection()){
+            
+          PreparedStatement ps = con.prepareStatement(query);
+          ps.setInt(1, id);
+          
+          ResultSet rs = ps.executeQuery();
+          
+          if( rs.next() && (rs.getString(2)) != null){   
+              String[] fieldNames = {"Name","NIC","Age","Contact","Gender","Department"};
+              for(int i =2; i<=7;i++){
+                  System.out.println((i == 4 || i == 5) ? "     "+ (i-1) + " " + fieldNames[i-2] + "-> " + rs.getInt(i) : "     "+ (i-1) + " " + fieldNames[i-2] + "-> " + rs.getString(i));
+              }
+              
+              System.out.print("\n   Which field you want to edit: ");
+              int choice = input.nextInt();
+              
+              String updateSql = "Update Students set "+ fieldNames[choice-1] + " = ? where Student_id = ?;";
+              PreparedStatement ps2 = con.prepareStatement(updateSql);
+              System.out.print("   Enter the " + fieldNames[choice-1] + " : ");
+              if (choice == 4 || choice == 5) {
+                int data = input.nextInt(); 
+                ps2.setInt(1, data);
+                ps2.setInt(2, id);
+              } else {
+                String data = input.nextLine();
+                 data = input.nextLine();
+                ps2.setString(1, data);
+                ps2.setInt(2, id);
+              }
+              ps2.executeUpdate();
+              
+              System.out.print("\n Successfully Updated!!!\n Press any letter to go to dashboard ");
+                 String selection= input.nextLine();
+                 if(selection != null){
+                    System.out.print("\033[H\033[2J");
+                    title();
+                    dashboard();
+                 }
+              
+          }else{
+            System.out.print("\n  Ops! That Student Id does\'t exist do you want to try again press \"y\" or press any letter to go back to dashboard: ");
+                String selection= input.nextLine();
+                selection= input.nextLine();
+                if(selection.equalsIgnoreCase("y")){
+                    System.out.print("\033[H\033[2J");
+                    title();
+                    editStudent();
+                }
+                else {
+                    System.out.print("\033[H\033[2J");
+                    title();
+                    dashboard();
+                 }
+          }
+        }catch(Exception e){
+            System.out.println("Error:" + e);
+        } 
+
+        }
+}
